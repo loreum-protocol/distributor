6f5313e674e03ef06e7ad0bffb72c30b98bb0df7
Satoshi Amemiya <satoshi_amemiya@voyagegroup.com>
2022-04-06 04:54:58 +0900

    decrypt

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,39 @@
-use x25519_dalek::{PublicKey, EphemeralSecret};
-use rand_chacha::ChaCha20Rng;
+use chacha20poly1305::aead::{Aead, NewAead};
+use chacha20poly1305::{ChaCha20Poly1305, Key, Nonce};
+use rand::prelude::*;
 use rand_chacha::rand_core::SeedableRng;
-use sha2::{Sha256, Digest};
+use rand_chacha::ChaCha20Rng;
+use sha2::{Digest, Sha256};
+use x25519_dalek::{EphemeralSecret, PublicKey};
 
-
-struct Message {
+pub struct Message {
+    nonce: Nonce,
     public_key: PublicKey,
     body: Vec<u8>,
 }
 
 static KEY: &[u8; 4] = b"hoge";
 
-fn private_key() -> EphemeralSecret {
+pub fn private_key() -> EphemeralSecret {
     let mut hasher = Sha256::new();
     hasher.update(KEY);
     let hash = hasher.finalize();
     EphemeralSecret::new(ChaCha20Rng::from_seed(hash.into()))
+}
+
+pub fn random_nonce() -> Nonce {
+    let mut bytes = [0; 12];
+    let mut rng = rand::thread_rng();
+    rng.fill_bytes(&mut bytes);
+    Nonce::from(bytes)
+}
+
+pub fn decrypt(key: EphemeralSecret, message: &Message) -> Vec<u8> {
+    let shared_key = key.diffie_hellman(&message.public_key);
+    let cipher = ChaCha20Poly1305::new(Key::from_slice(shared_key.as_bytes()));
+    cipher
+        .decrypt(&message.nonce, message.body.as_slice())
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -23,18 +41,26 @@ mod test {
     use super::*;
 
     fn dummy_message() -> Message {
-        let mut bytes = [0; 32];
-        bytes.copy_from_slice(&base64::decode("79ToCqhhtYcrsysTavwL+DzhwfQ8WEftzXrOI0LV+XY=").unwrap());
+        let mut nonce = [0; 12];
+        nonce.copy_from_slice(&base64::decode("VtdcQo0osZno7E00").unwrap());
+
+        let mut public_key = [0; 32];
+        public_key.copy_from_slice(
+            &base64::decode("79ToCqhhtYcrsysTavwL+DzhwfQ8WEftzXrOI0LV+XY=").unwrap(),
+        );
+
         Message {
-            public_key: PublicKey::from(bytes),
-            body: Vec::new(),
+            nonce: Nonce::from(nonce),
+            public_key: PublicKey::from(public_key),
+            body: base64::decode("e1qeUfZLTTdNBw0Y8u1C8dRZ6fU=").unwrap(),
         }
     }
 
     #[test]
-    fn test_shared_key() {
+    fn test() {
         let message = dummy_message();
         let private_key = private_key();
-        let shared_key = private_key.diffie_hellman(&message.public_key);
+        let result = decrypt(private_key, &message);
+        assert_eq!(&String::from_utf8_lossy(&result), "hoge");
     }
 }
